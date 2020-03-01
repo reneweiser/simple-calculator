c749ecf6c88fbf8fcb69c3ecdfa98905a0d98950
René Weiser <rene.weiser@uni-weimar.de>
2020-03-01 14:01:42 +0100

    Adding basic and chained operations. Chaining only works using equals

--- main.js
+++ main.js
@@ -1,49 +1,102 @@
 function add(a, b) {
-    return a + b;
+    return (parseFloat(a, 10) + parseFloat(b, 10)).toPrecision(5);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return (a - b).toPrecision(5);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return (a * b).toPrecision(5);
 }
 
 function divide(a, b) {
-    return a / b;
+    return (a / b).toPrecision(5);
 }
 
 function operate(operator, a, b) {
+    let result = null;
     switch(operator)
     {
         case '+':
-            console.log(add(a,b));
+            result = add(a,b);
             break;
         case '-':
-            console.log(subtract(a,b));
+            result = subtract(a,b);
             break;
         case '*':
-            console.log(multiply(a,b));
+            result = multiply(a,b);
             break;
         case '/':
-            console.log(divide(a,b));
+            result = divide(a,b);
             break;
     }
+    return result;
+}
+
+function putDigit(digit) {
+    if (input.lhs === null)
+        input.lhs = digit;
+    else
+        input.rhs = digit;
 }
 
 const calculatorInput = document.querySelector('#calculator-input');
-const keyContainer = document.querySelector('#keys');
-const keys = '789/456*123-0.=+'.split('');
+const digitContainer = document.querySelector('#digits');
+const operatorContainer = document.querySelector('#operators');
+const keys = '7894561230'.split('');
+const operators = '/*-+'.split('');
+let input = {
+    lhs: null,
+    rhs: null,
+    operator: null
+};
 
 keys.forEach(key => {
     let currentNode = document.createElement('button');
     currentNode.className = 'digit';
     currentNode.innerText = key;
-    keyContainer.appendChild(currentNode);
+    currentNode.addEventListener('click', () => calculatorInput.value = `${calculatorInput.value}${key}`);
+    digitContainer.appendChild(currentNode);
 });
+
+operators.forEach(key => {
+    let currentNode = document.createElement('button');
+    currentNode.className = 'digit';
+    currentNode.innerText = key;
+    currentNode.addEventListener('click', () => {
+        putDigit(calculatorInput.value);
+        input.operator = key;
+        calculatorInput.value = '';
+    });
+    operatorContainer.appendChild(currentNode);
+});
+
+let decimalButton = document.createElement('button');
+decimalButton.className = 'digit';
+decimalButton.innerText = '.';
+decimalButton.addEventListener('click', () => calculatorInput.value = `${calculatorInput.value}.`);
+digitContainer.appendChild(decimalButton);
+
+let evaluateButton = document.createElement('button');
+evaluateButton.className = 'digit';
+evaluateButton.innerText = '=';
+evaluateButton.addEventListener('click', () => {
+    putDigit(calculatorInput.value);
+    input.lhs = operate(input.operator, input.lhs, input.rhs);
+    calculatorInput.value = input.lhs;
+});
+digitContainer.appendChild(evaluateButton);
 
 let clearButton = document.createElement('button');
 clearButton.className = 'digit';
 clearButton.innerText = 'AC';
-keyContainer.appendChild(clearButton);
+clearButton.addEventListener('click', () => {
+    input = {
+        lhs: null,
+        rhs: null,
+        operator: null
+    };
+    calculatorInput.value = '';
+});
+document.querySelector('#functions').appendChild(clearButton);
